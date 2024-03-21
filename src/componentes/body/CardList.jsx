74e15de71b0289e668f60105e5408aed3f6884f1
Camilo <137653825+CXmiloxx@users.noreply.github.com>
2024-03-20 19:42:16 -0500

    mejoras en img y body

---
+++ src/componentes/body/CardList.jsx
@@ -0,0 +1,19 @@
+import Cards from './Cards';
+import data from './Data';
+
+function CardList(){
+    const cards = data.map(items => {
+        return(
+            <Cards key={items.id} items={items}/>
+        )
+    })
+    return(
+        <div>
+            <div className='divCards'>
+                {cards}
+            </div>
+        </div>
+    )
+}
+
+export default CardList;
---
+++ src/componentes/body/Data.jsx
@@ -0,0 +1,135 @@
+import madrid from './madrid.jpeg';
+import liverpool from './liverpool.jpeg';
+import bayer from './bayer.jpeg';
+import barcelona from  './barcelona.jpeg';
+import boca from './boca.jpeg';
+import manchesterUnited from './manchesterUnited.jpeg';
+import juventus from './juventus.jpeg';
+import milan from './milan.jpeg';
+import porto from './porto.jpeg';
+import santos from './santos.jpeg';
+import ajax from './ajax.jpeg';
+import chelsea from './chelsea.jpeg';
+
+export default [
+    {
+        id:1,
+        title: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        fechaLanzamiento:"20-12-1980",
+        description: "Cien años de soledad se ha convertido en la novela más emblemática de la literatura latinoamericana",
+        image: liverpool,
+        precio:"$1000"
+    },
+
+    {
+        id:2,
+        title: "COSMOS",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1980",
+        description: "Cosmos trata de la ciencia en un contexto humano más amplio y explica como la ciencia y la ciencia ",
+        image: madrid,
+        precio:"$25000"
+    },
+
+    {
+        id:3,
+        title: "El coronel no tiene quien le escriba",
+        autor: "Gabriel Garcá Márquez",
+        fechaLanzamiento:"20-12-1979",
+        description: "La vida",
+        image: bayer,
+        precio:"$25000"
+    },
+
+    {
+        id:4,
+        title: "El amor en los tiempos de cólera",
+        autor: "Gabriel Garcá Márquez",
+        fechaLanzamiento:"20-12-1995",
+        description: "La vida",
+        image: barcelona,
+        precio:"$25000"
+    },
+
+    {
+        id:5,
+        title: "Todos los cuentos",
+        autor: "Gabriel Garcá Márquez",
+        fechaLanzamiento:"20-12-1995",
+        description: "La vida",
+        image: ajax,
+        precio:"$25000"
+    },
+
+    {
+        id:6,
+        title: "Contacto",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: chelsea,
+        precio:"$25000"
+    },
+
+    {
+        id:7,
+        title: "Cerebro de Broca",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: manchesterUnited,
+        precio:"$25000"
+    },
+
+    {
+        id:8,
+        title: "Los miserables",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: juventus,
+        precio:"$25000"
+    },
+
+    {
+        id:9,
+        title: "Los cuatro acuerdos",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: boca,
+        precio:"$25000"
+    },
+
+    {
+        id:10,
+        title: "La lógica de la mentira",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: milan,
+        precio:"$25000"
+    },
+
+    {
+        id:11,
+        title: "Diccionario de ajedrez",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: porto,
+        precio:"$25000"
+    },
+
+    {
+        id:12,
+        title: "El poder de las palabras",
+        autor: "Sigman Mariano",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: santos,
+        precio:"$25000"
+    }
+
+]
