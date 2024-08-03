3644fd17c0469e6b02be07f4a2b90f5678be5749
Camilo <137653825+CXmiloxx@users.noreply.github.com>
2024-08-03 18:43:24 -0500

    instalacion de dependencias

--- server/config/configBD.js
+++ server/config/configBD.js
@@ -1,12 +1,21 @@
 import mysql from 'mysql2/promise';
 
-const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bdLocal'
-});
+async function createConnection() {
+    try {
+        const connection = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'bdLocal'
+        });
+        console.log('Conexión exitosa a la base de datos');
+        return connection;
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        throw error;
+    }
+}
 
-console.log('Conexión exitosa a la base de datos');
+const connection = createConnection();
 
 export default connection;
