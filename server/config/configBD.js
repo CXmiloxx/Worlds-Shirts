0c2a21083fe8991a9d4631ac1a5313e642fb0400
Camilo <137653825+CXmiloxx@users.noreply.github.com>
2024-08-04 10:44:44 -0500

    instalacion de dependencias y cambios en estrucutra de bd

--- server/config/configBD.js
+++ server/config/configBD.js
@@ -1,14 +1,19 @@
+/* eslint-disable no-undef */
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 let connection;
 
 export async function createConnection() {
     if (!connection) {
+        const password = process.env.DB_PASSWORD === 'NO_PASSWORD' ? '' : process.env.DB_PASSWORD;
         connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'worldShirt'
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: password,
+            database: process.env.DB_NAME
         });
         console.log('Conexión exitosa a la base de datos');
     }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
 import express from 'express';
-const app = express();
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import user from './controller/userRoutes.js'
+import dotenv from 'dotenv';
+import user from './controller/userRoutes.js';
+
+dotenv.config();
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/", user);
 
-
-
 const PORT = 3001;
 app.listen(PORT, () => {
-
   console.log("Servidor corriendo en el puerto ", PORT);
-
 });
