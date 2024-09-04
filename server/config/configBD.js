04caada098e4d2a680f438a2b4380cb1630c6bee
Camilo <juancamilog9911@gmail.com>
2024-09-04 13:21:01 -0500

    cambios en bd

--- server/config/configBD.js
+++ server/config/configBD.js
@@ -1,15 +1,20 @@
 /* eslint-disable no-undef */
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { MYSQL_ADDON_HOST,MYSQL_ADDON_DB,MYSQL_ADDON_USER, MYSQL_ADDON_PASSWORD } = process.env;
 
 let connection;
 
 export async function createConnection() {
     if (!connection) {
         connection = await mysql.createConnection({
-            host : 'bkfaj2n1j7fpijaf13h1-mysql.services.clever-cloud.com',
-            user: 'utypeya7dj32gwxg',
-            password: 'HuhJUO89sGTxrz2fW5hV',
-            database : 'bkfaj2n1j7fpijaf13h1',
+            host : MYSQL_ADDON_HOST,
+            user: MYSQL_ADDON_USER,
+            password: MYSQL_ADDON_PASSWORD,
+            database : MYSQL_ADDON_DB,
         });
         console.log('Conexión exitosa a la base de datos');
     }
