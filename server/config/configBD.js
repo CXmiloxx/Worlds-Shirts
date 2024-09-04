5233f00aaf66bac1855bd4b2d6ce30028ac56d68
Camilo <juancamilog9911@gmail.com>
2024-09-04 12:29:49 -0500

    cambios en la bd

--- server/config/configBD.js
+++ server/config/configBD.js
@@ -8,12 +8,12 @@ let connection;
 
 export async function createConnection() {
     if (!connection) {
-        const password = process.env.DB_PASSWORD || '' ;
+        const password = process.env.VITE_APP_DB_PASSWORD || '' ;
         connection = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
+            host: process.env.VITE_APP_DB_HOST,
+            user: process.env.VITE_APP_DB_USER,
             password: password,
-            database: process.env.DB_NAME
+            database: process.env.VITE_APP_DB_NAME,
         });
         console.log('Conexión exitosa a la base de datos');
     }
